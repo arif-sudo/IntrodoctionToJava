cb055931b67ef3f09fdfdb04fba5307f82ae7e40
Arif <arif520gg@gmail.com>
2023-09-17 14:19:28 +0400

    Types intro

--- src/Types17_09/Type.java
+++ src/Types17_09/Type.java
@@ -1,5 +1,11 @@
 package Types17_09;
 
+// primitive
+//      8 types: int, String, boolean, short, byte, float, double etc.
+//      stores data
+// reference
+//      unlimited (user defined)
+//      stores an adderess
 public class Type {
     public static void main(String[] args) {
         System.err.println("Type");
