513d85068a96a5c1729599d24f4bef780b3f95a5
Arif <arif520gg@gmail.com>
2023-10-13 00:46:15 +0400

    Data Structures Java Subarray

---
+++ src/HRank/Subarray.java
@@ -0,0 +1,43 @@
+package HRank;
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Subarray {
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        int arr[] = new int[n];
+        int count = 0;
+
+        for (int i = 0; i < n; i++) {
+            int k = scan.nextInt();
+            arr[i] = k;
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n ; j++) {
+                int[] newArr = new int[j+1];
+                for (int k = 0 + i; k <= j; k++) {
+                    newArr[k] = arr[k];
+                }
+                if (sum(newArr) < 0){
+                    count++;
+                }
+            }
+        }
+        System.out.println(count);
+    }
+    public static int sum(int[] arr){
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+        }
+        return sum;
+    }
+}
+// https://www.hackerrank.com/challenges/java-negative-subarray/problem?isFullScreen=
